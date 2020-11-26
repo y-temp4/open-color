7ce0171580281e626c4d5c5a6adadcc7c699ad15
y-temp4 <y.temp4@gmail.com>
2020-11-26 17:28:56 +0900

    Remove useless code

--- builder/util/string.js
+++
@@ -1,5 +0,0 @@
-function capitalize(text){
-    return text.charAt(0).toUpperCase() + text.slice(1);
-}
-
-module.exports = {capitalize};
--- tests/typescript.ts
+++
@@ -1,9 +0,0 @@
-import OpenColor = require("open-color");
-
-// black is regular string field
-OpenColor.black.substring(1);
-
-// red is array string field
-OpenColor.red.forEach(hex => {
-    console.log(hex.substring(1));
-});
